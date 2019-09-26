78b56c20646b1c3e2d058fe814202e9b5e3cea37
Manav Misra <manavm@visionwebsoft.com>
2019-09-25 19:45:09 -0500

    Show some silly code to illustrate undefined and NOT DEFINED error.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,19 @@
 // TODO: Sum the numbers from 1 to 10
-// keyword - expression in parentheses - {}
-let sum = 0;
+
+// Function Expression - we specify NAMED PARAMETERS which are assigned values based on ARGUMENTS that we pass upon INVOCATION
+function numberSummer(start, end) {
+  console.log('start value is:', start);
+  console.log('end value type is:', typeof end);
+
+
+}
+
+numberSummer();
 
 // `for` allows us to lose 1 global variable which becomes managed by the `for` loop.
 // (initializer/declaration; exit condition; iteration;)
-for(let i = 1; i <= 10; i += 1) {
-  sum += i;
-}
+// for(let i = 1; i <= 10; i += 1) {
+//   sum += i;
+// }
 
-console.log(sum);
+// console.log(sum);
